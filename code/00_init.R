06aa177e01b455c938d95f6d4185da7ecf364b51
George Kvetenadze <georgekvetenadze@MacBook-Air-George.local>
2022-03-21 17:12:10 +0100

    hw 1

--- code/00_init.R
+++
@@ -1,5 +0,0 @@
-dir.create('./data')
-dir.create('./data/raw')
-dir.create('./results')
-dir.create('./results/figures')
-dir.create('./docs')
---
+++ code/EDA 1.R
@@ -0,0 +1,27 @@
+dir.create('./data')
+dir.create('./data/raw')
+dir.create('./results')
+dir.create('./results/figures')
+dir.create('./docs')
+
+runoff <- 130
+
+runoff_ts <- data.frame(time = as.Date(1:90, origin = '2020/12/31'), 
+                        value = sample(c(130, 135, 140), 
+                                       size = 90, replace = T))
+head(runoff_ts)
+library(data.table)
+runoff_dt <- data.table(runoff_ts)
+runoff_dt[value > 130]
+runoff_dt[value > 130, mean(value)]
+runoff_dt[value > 130, mean(value), by = month(time)]
+runoff_dt[, mon := month(time)]
+#new column
+runoff_dt[, week := week(time)]
+runoff_dt[, mon_mean := mean(value), by = mon]
+mon_mean <- runoff_dt[,mean(value, by = mon)]
+runoff_month <- runoff_dt[, .(mon, mon_mean)]
+runoff_month
+unique(runoff_month)
+saveRDS(runoff_dt, file = "./data/dt_example.rds")
+gitcreds::gitcreds_set()                          
